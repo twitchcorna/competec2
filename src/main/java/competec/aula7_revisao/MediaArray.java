652da2fe46e3c0d6d912299ab5c49bd80a93b5bd
twitchcorna <166771282+twitchcorna@users.noreply.github.com>
2024-08-24 09:51:25 -0300

    Update MediaArray.java

--- src/main/java/competec/aula7_revisao/MediaArray.java
+++ src/main/java/competec/aula7_revisao/MediaArray.java
@@ -5,6 +5,8 @@ package competec.aula7_revisao;
 public class MediaArray {
     public static void main(String[] args) {
         int[] numeros = {10, 20, 30, 40, 50}; // Modifique os valores conforme necessário
+        int media = (numeros[0] + numeros[1] + numeros[2] + numeros[3] + numeros[4]) / numeros.length;
+      System.out.println("a media e " + media);
 
         // Calcule a média dos números
     }
